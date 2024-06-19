ffa9dda4750858a1684e75400f984d5ecfd4a763
zahrabagheri1 <zahrabagheriuk@gmail.com>
2024-06-19 20:20:59 +0330

    start mui

---
+++ Mui/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Layout/Header/index";
+import Footer from "./components/Layout/Footer/index";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
