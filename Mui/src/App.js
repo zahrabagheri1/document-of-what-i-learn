73ae138ef52d9539f60880abf5af4f0c1eead2eb
zahrabagheri1 <zahrabagheriuk@gmail.com>
2024-06-20 15:18:19 +0330

    add App Bar 🍹

--- Mui/src/App.js
+++ Mui/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./components/Layout/Header/index";
 import Footer from "./components/Layout/Footer/index";
+import Home from "./pages/Home/index";
 
 function App() {
   return (
     <>
       <Header />
-
+      <Home />
       <Footer />
     </>
   );
---
+++ Mui/src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>Home Page</div>
+  )
+}
+
+export default index
