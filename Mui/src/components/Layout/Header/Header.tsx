d2c1ceef7174dbfedca74dfbaa7c9438e0374179
zahrabagheri1 <zahrabagheriuk@gmail.com>
2024-06-23 20:16:47 +0330

    header and footer

--- Mui/src/components/Layout/Header/Header.tsx
+++ Mui/src/components/Layout/Header/Header.tsx
@@ -1,14 +1,20 @@
-import { AppBar, Stack } from '@mui/material'
+import { AppBar, Button, Container, Stack } from '@mui/material'
 import React from 'react'
 
 function Header() {
     return (
-        <>
-            <AppBar color={'transparent'} elevation={0}>
-                {/* <AppBar> is header component </AppBar> */}
-                {/* div => Stack and Box  */}
-                {/* box has elevation  */}
-                <Stack>
+        <AppBar color={'transparent'} elevation={0} position='static'>
+            {/* <AppBar> is header component </AppBar> */}
+            {/* div => Stack and Box  */}
+            {/* box has elevation  */}
+            {/* stack direction flex defult is colem */}
+            {/* box direction flex defult is row */}
+            <Container maxWidth={'lg'}>
+                <Stack
+                    flexDirection={'row'}
+                    justifyContent={'space-between'}
+                    alignItems={'center'}
+                >
                     <Stack>
                         <img
                             alt='gelato logo'
@@ -16,10 +22,18 @@ function Header() {
                             width={60}
                             src={'https://order.gelatohouse.ir/2.15.13/img/9dcd0541179a7fde025e88026901a4be.png'} />
                     </Stack>
-                    <Stack></Stack>
+                    <Stack flexDirection={'row'}>
+                        <Stack flexDirection={'row'} gap={2}>
+                            <Button >سفارش اینترنتی</Button>
+                            <Button >قوانین سایت</Button>
+                        </Stack>
+                        <Stack color={'primary'}>
+                            <Button variant='contained'>ورود / عضویت</Button>
+                        </Stack>
+                    </Stack>
                 </Stack>
-            </AppBar>
-        </>
+            </Container>
+        </AppBar>
     )
 }
 
