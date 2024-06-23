e2999a70ae14d7b8a89aae83ead3eed27a4f3f9c
zahrabagheri1 <zahrabagheriuk@gmail.com>
2024-06-23 20:17:32 +0330

    axios

---
+++ Mui/src/constants/apiUrl.ts
@@ -0,0 +1,7 @@
+export const API_URL = {
+  GetSliders:
+    "subscriber/sliders/10tQStiKTniALgYpYQ4hm0UCuadXWbHdMklMIpyTE5DSzkNSfx1r2p02pqg3QKx3",
+};
+
+// https://restaurant.delino.com/subscriber/sliders/10tQStiKTniALgYpYQ4hm0UCuadXWbHdMklMIpyTE5DSzkNSfx1r2p02pqg3QKx3
+// https://restaurant.delino.com/restaurant/foodtag/10tQStiKTniALgYpYQ4hm0UCuadXWbHdMklMIpyTE5DSzkNSfx1r2p02pqg3QKx3
