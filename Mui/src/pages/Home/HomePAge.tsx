a43bc31b89e38ba2aac09eef8cb842133c2b8595
zahrabagheri1 <zahrabagheriuk@gmail.com>
2024-06-23 20:17:12 +0330

    home page

--- Mui/src/pages/Home/HomePAge.tsx
+++ Mui/src/pages/Home/HomePAge.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Slider from '../../components/Basic/Slider/Index'
+import { useAxios } from '../../hook/useAxios'
+import { API_URL } from '../../constants/apiUrl'
+import { Skeleton } from '@mui/material'
 
 function HomePAge() {
+
+  const { data, isLoading } = useAxios<any, GetSliderResponse[], any>({
+    url: API_URL.GetSliders
+  })
+
+  if (isLoading) {
+    return <Skeleton height={"50vh"} animation={"wave"} />
+  }
+
   return (
-    <div>HomePAge</div>
+    <>
+      <Slider images={data?.map(item => item.src) as string[]} />
+    </>
   )
 }
 
