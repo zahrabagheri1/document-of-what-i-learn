07761a7a420663d4d7685d532dcf51a8bdfbd963
zahrabagheri1 <zahrabagheriuk@gmail.com>
2024-07-03 16:16:19 +0330

    nav bar

--- zibagram/src/App.tsx
+++ zibagram/src/App.tsx
@@ -1,8 +1,20 @@
+import Navbar from "./components/global/navbar"
 
 function App() {
   return (
-    <div className="bg-slate-300 min-h-screen font-oswald">
-      Zibageram
+    <div className="bg-slate-200 min-h-screen font-oswald">
+      <Navbar />
+
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="w-[80%] rounded-[20px] bg-blue-100 p-2 border border-solid border-blue-400 flex flex-row justify-between">
+          <div className="w-[50%] flex flex-col items-center justify-between">
+            <div className="">Login page</div>
+          </div>
+          <div className="w-[50%]">
+            <img width={550} height={600} src={'https://media.mktg.workday.com/is/image/workday/illustration-people-login?fmt=png-alpha&wid=1000'} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ zibagram/src/components/global/navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <header className="fixed bg-white p-8 shadow-md w-full">
+        <h1 className="text-3xl text-blue-700">Zibageram</h1>
+      </header>
+    )
+}
