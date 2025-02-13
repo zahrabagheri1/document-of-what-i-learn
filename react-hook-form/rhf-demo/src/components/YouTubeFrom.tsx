3ee5d941fcf40306e0296d79dfc4e87340ebcef4
zahrabagheri1 <zahrabagheriuk@gmail.com>
2025-02-14 01:15:58 +0330

    Form Submission

--- react-hook-form/rhf-demo/src/components/YouTubeFrom.tsx
+++ react-hook-form/rhf-demo/src/components/YouTubeFrom.tsx
@@ -4,19 +4,30 @@ import { DevTool } from '@hookform/devtools';
 
 let renderCount = 0;
 
+type FormData = {
+  username: string;
+  email: string;
+  channel: string;
+}
+
 function YouTubeFrom() {
-  const form = useForm();
-  const { register, control } = form;
+  const form = useForm<FormData>();
+  const { register, control, handleSubmit } = form;
   // const { name, ref, onChange, onBlur } = register('username');
+
+  const onSubmit = (data: FormData) => {
+    console.log("Form Submitted", data)
+  }
 
   renderCount++;
   return (
     <div>
-      <h1>YouTube From ({renderCount/2})</h1>
-      <form>
+      <h1>YouTube From ({renderCount / 2})</h1>
+      
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" 
-        {...register('username')}
+        <input type="text" id="username"
+          {...register('username')}
         // name={name} ref={ref} onChange={onChange} onBlur={onBlur} 
         />
 
