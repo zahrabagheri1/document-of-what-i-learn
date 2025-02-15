f2eb3e913baaed8e47b36e2071c151597f6bc42a
zahrabagheri1 <zahrabagheriuk@gmail.com>
2025-02-15 23:17:40 +0330

    Query by Id

--- react-query/react-query-demo/src/hooks/useSuperHeroData.js
+++ react-query/react-query-demo/src/hooks/useSuperHeroData.js
@@ -2,6 +2,11 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+// const fetchSuperHero = ({ queryKey }) => {
+//   const heroId = queryKey[1];
+//   return axios.get(`http://localhost:4000/superheroes/${heroId}`);
+// };
+
 const fetchSuperHero = (heroId) => {
   return axios.get(`http://localhost:4000/superheroes/${heroId}`);
 };
