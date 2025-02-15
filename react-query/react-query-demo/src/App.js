6b7e3df7bf59a1cf3c3da6cc680c73ee0794b65c
zahrabagheri1 <zahrabagheriuk@gmail.com>
2025-02-15 16:08:39 +0330

    Fetching Data with useQuery

--- react-query/react-query-demo/src/App.js
+++ react-query/react-query-demo/src/App.js
@@ -1,33 +1,38 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import "./App.css";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import HomePage from "./components/HomePage";
 import RQSuperHeroesPage from "./components/RQSuperHeroesPage";
 import SuperHeroesPage from "./components/SuperHeroesPage";
+import "./App.css";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <Router>
-      <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/super-heroes">Traditional Super Heroes</Link>
-            </li>
-            <li>
-              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/super-heroes" element={<SuperHeroesPage />} />
-          <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="App">
+          <nav className="nav-bar">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/super-heroes">Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/super-heroes" element={<SuperHeroesPage />} />
+            <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
--- react-query/react-query-demo/src/components/RQSuperHeroesPage.js
+++ react-query/react-query-demo/src/components/RQSuperHeroesPage.js
@@ -1,6 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
 function RQSuperHeroesPage() {
+  const { isLoading, data } = useQuery({
+    queryKey: ["super-heroes"],
+    //   queryFn: () => {
+    //     return axios.get("http://localhost:4000/superheroes");
+    //   },
+    queryFn: fetchSuperHeroes,
+  });
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
-    <h1>RQ Super Heroes Page</h1>
+    <>
+      <h1>RQ Super Heroes Page</h1>
+      {data?.data.map((hero) => (
+        <div key={hero.name}>{hero.name}</div>
+      ))}
+    </>
   );
 }
 
