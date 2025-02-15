f65951fe05230698bf427ab002334ce2e117e13e
zahrabagheri1 <zahrabagheriuk@gmail.com>
2025-02-15 07:39:29 +0330

    just with react hook form

--- react-hook-form/loginForm/src/App.tsx
+++ react-hook-form/loginForm/src/App.tsx
@@ -1,58 +1,92 @@
-
 import { useState } from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
 import './App.css'
 
+type FormFields = {
+  email: string
+  password: string
+}
+
 function App() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [errors, setErrors] = useState<{ email: string, password: string }>({
-    email: "",
-    password: ""
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting }
+  } = useForm<FormFields>({
+    defaultValues: {
+      email: 'test@test.com',
+      password: 'test12345678'
+    },
+    resolver: zodResolver(schema)
   })
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    setErrors({ email: "", password: "" })
-
-    //manual validation
-    if (!email.includes("@")) {
-      setErrors({ ...errors, email: "Email must contain @ symbol" })
-      return;
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    try {
+      await new Promise(resolve => setTimeout(resolve, 1000))
+      console.log(data)
+      throw new Error()
+    } catch (error) {
+      setError('root', {
+        message: `This ${error.message} is already in taken`
+      })
     }
-
-    if (password.length < 8) {
-      setErrors({ ...errors, password: "Password must be at least 8 characters long" })
-      return;
-    }
-
-    //Submit form
-    console.log("Form submitted")
   }
+
 
   return (
     <>
       <h1>Login Form</h1>
-      <form className="form gap-2" onSubmit={handleSubmit}>
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
 
+        {/* Email */}
         <div className="form-control">
-          <input className="form-input" type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
           <label htmlFor="email" className="form-label">Email</label>
-          {errors.email && <p className='error'>{errors.email}</p>}
+          <input className="form-input"
+            type="text"
+            placeholder="Email"
+            {...register("email",
+              {
+                required: 'Email is required',
+                // pattern: {
+                //   value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                //   message: "Invalid email address"
+                // }
+                validate: (value) => !value.includes('@') ? 'Invalid email address' : true
+              },
+            )}
+          />
+          {errors.email && <p className="form-error">{errors.email.message}</p>}
         </div>
 
+        {/* Password */}
         <div className="form-control">
-          <input className="form-input" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
           <label htmlFor="password" className="form-label">Password</label>
-          {errors.password && <p className='error'>{errors.password}</p>}
+          <input className="form-input"
+            type="password"
+            placeholder="Password"
+            {...register("password",
+              {
+                required: 'Password is required',
+                minLength: {
+                  value: 8,
+                  message: 'Password must be at least 8 characters long'
+                }
+              }
+            )}
+          />
+          {errors.password && <p className="form-error">{errors.password.message}</p>}
         </div>
 
-        <div className="form-button-container">
-          <p className="form-button-text">Click button to login</p>
-          <button className="form-button" type="submit">
-            <span className="back"></span>
-            <span className="front"></span>
-          </button>
-        </div>
+
+        {/* Button */}
+
+        <button className="button" type="submit" disabled={isSubmitting}>
+          <span className="button-text">{isSubmitting ? 'Submitting...' : 'Submit'}</span>
+        </button>
+        {errors.root && <p className="form-error">{errors.root.message}</p>}
 
       </form>
     </>
